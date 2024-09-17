fd93929165bb607990106becda84b6a155944272
hyeongjun <sx0527@naver.com>
2024-09-18 00:08:40 +0900

    fix: rollback removed api functions on PR#15
    Co-authored-by: xiyoon <ryuu.public@gmail.com>

--- src/api/api.js
+++ src/api/api.js
@@ -19,6 +19,34 @@ export const getToken = () => {
   return localStorage.getItem('jwtToken');
 };
 
+
+/**
+  * API를 호출하여 사용자의 로그인 정보를 확인하고 JWT 토큰을 반환하는 함수.
+  * @param {string} email - 사용자의 이메일 주소
+  * @param {string} password - 사용자의 비밀번호
+  * @returns {Promise} 로그인 결과에 따라 JWT 토큰을 포함한 응답 또는 오류
+  */
+export const login = async (email, password) => {
+  try {
+    const response = await axios.post(`${API_ORIGIN}/api/auth/token`, { email, password });
+    const token = response.data.accessToken
+    console.log('accessToken:', token);
+    return token;
+  } catch (error) {
+    console.error('Login failed:', error);
+    throw error;
+  }
+};
+
+/**
+ * OAuth2 소셜 로그인 URL을 반환하는 함수.
+ * @param {string} provider - 소셜 로그인 제공자 (예: 'google', 'naver', 'kakao')
+ * @returns {string} 소셜 로그인 URL
+ */
+export const getOAuth2LoginUrl = (provider) => {
+  return `${API_ORIGIN}/oauth2/authorization/${provider}`;
+};
+
 /**
  * 진행 중인 이벤트 목록을 가져오는 함수.
  * @returns {Promise} 진행 중인 이벤트 목록 또는 오류
