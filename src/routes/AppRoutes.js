51461373fa910abaf64e2b2b141e6971c9e86c43
hyeongjun <sx0527@naver.com>
2024-09-17 12:32:31 +0900

    feat: initialize React project and implement basic WebSocket client
    - Set up project structure with create-react-app
    - Add zustand and react-use-websocket dependencies
    - Create useChatStore for state management
    - Implement useChatWebSocket hook for WebSocket connection
    - Develop Chat component for sending and displaying messages
    - Write unit tests for Chat component
    - Refactor directory name (store -> stores)

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -5,6 +5,7 @@ import Login from '../pages/Login';
 import MainLayout from '../layouts/MainLayout';
 import AuthLayout from '../layouts/AuthLayout';
 import Maintenance from '../components/MaintenancePage';
+import Chat from '../components/Chat';
 
 const AppRoutes = () => {
   return (
@@ -12,7 +13,7 @@ const AppRoutes = () => {
       {/* 인증 레이아웃이 필요한 경우 */}
       <Route path="/" element={<MainLayout><Maintenance/></MainLayout>} />
       <Route path="/login" element={<AuthLayout><Login /></AuthLayout>} /> 
-      
+      <Route path="/chat" element={<Chat />} />
       {/* 다른 페이지 경로 추가 */}
     </Routes>
   );
